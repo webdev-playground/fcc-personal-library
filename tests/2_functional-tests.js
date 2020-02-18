fe6c3e33b1ba5e88c512e577a0276cfbcc8fecfb
Glitch (freecodecamp-boilerplate-project-library-5) <none>
2020-02-18 15:05:19 +0000

    🏁🚂 Checkpoint ./tests/2_functional-tests.js:11249790/1704 ./package.json:11249790/36

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -13,24 +13,27 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
+let newId;
+const fakeId = '5e4bf9f4c50c385c3a2a9aaa';
+
 suite('Functional Tests', function() {
 
   /*
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
   */
-  test('#example Test GET /api/books', function(done){
-     chai.request(server)
-      .get('/api/books')
-      .end(function(err, res){
-        assert.equal(res.status, 200);
-        assert.isArray(res.body, 'response should be an array');
-        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-        assert.property(res.body[0], 'title', 'Books in array should contain title');
-        assert.property(res.body[0], '_id', 'Books in array should contain _id');
-        done();
-      });
-  });
+  // test('#example Test GET /api/books', function(done){
+  //    chai.request(server)
+  //     .get('/api/books')
+  //     .end(function(err, res){
+  //       assert.equal(res.status, 200);
+  //       assert.isArray(res.body, 'response should be an array');
+  //       assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+  //       assert.property(res.body[0], 'title', 'Books in array should contain title');
+  //       assert.property(res.body[0], '_id', 'Books in array should contain _id');
+  //       done();
+  //     });
+  // });
   /*
   * ----[END of EXAMPLE TEST]----
   */
@@ -44,7 +47,10 @@ suite('Functional Tests', function() {
         chai.request(server).post('/api/books').send({ title: 'ABC' }).end((err, res) => {
           assert.equal(res.status, 201);
           assert.equal(res.body.title, 'ABC');
-          assert.property(res, '_id');
+          assert.property(res.body, '_id');
+          
+          newId = res.body._id;
+          
           done();
         });
       });
@@ -63,7 +69,7 @@ suite('Functional Tests', function() {
       
       test('Test GET /api/books',  function(done){
         chai.request(server).get('/api/books').end((err, res) => {
-          assert.equal(res.status)
+          assert.equal(res.status, 200)
           assert.isArray(res.body);
           assert.property(res.body[0], '_id');
           assert.property(res.body[0], 'title');
@@ -78,12 +84,21 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        chai.request(server).get('/api/books').end((err, res) => {
-        //done();
+        chai.request(server).get('/api/books/' + fakeId).end((err, res) => {
+          assert.equal(res.status, 404);
+          done();
+        });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai.request(server).get('/api/books/' + newId).end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, '_id');
+          assert.equal(res.body._id, newId);
+          assert.property(res.body, 'title');
+          assert.property(res.body, 'comments');
+          done();
+        });
       });
       
     });
@@ -92,7 +107,15 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai.request(server).post('/api/books/' + newId).send({ comment: 'hello world' }).end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, '_id');
+          assert.equal(res.body._id, newId);
+          assert.property(res.body, 'title');
+          assert.property(res.body, 'comments');
+          assert.include(res.body.comments, 'hello world');
+          done();
+        });
       });
       
     });
